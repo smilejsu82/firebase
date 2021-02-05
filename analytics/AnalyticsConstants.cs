891cde16d87421cdb813f8214937a9c0c8d1c9d0
smilejsu <smilejsu82@gmail.com>
2021-02-05 11:56:05 +0900

    AnalyticsConstants.cs추가

---
+++ analytics/AnalyticsConstants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsConstants
+{
+    public const string EVENT_GUEST_LOGIN = "EventGuestLogin";
+    public const string EVENT_LEVEL_CLEAR = "EventLevelClear";
+}
